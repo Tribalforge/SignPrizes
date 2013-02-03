091d08f3e9e368af4d106ec4004012c339b5336c
Daniel Naylor <git@drnaylor.co.uk>
2013-02-03 13:57:41 +0000

    Initial commit

---
+++ src/main/java/uk/co/drnaylor/moneysigns/PlayerEventHandler.java
@@ -0,0 +1,15 @@
+package uk.co.drnaylor.moneysigns;
+
+import org.bukkit.event.Listener;
+
+public class PlayerEventHandler implements Listener {
+    
+    // Player Join - open MoneyUser class for him/her
+    
+    
+    // Player Quit - close MoneyUser class and save file
+    
+    
+    // PlayerInteractEvent - signs!
+    
+}
