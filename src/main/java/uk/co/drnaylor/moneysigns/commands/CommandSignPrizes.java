0f50f34f9ff2a1178345e37ec5a1471cd0eb09c3
Eevee Modevet <Eevables@hotmail.com>
2013-04-17 10:07:58 -0500

    Create CommandSignPrizes.java for the /sp command.
    This command will handle things such as prize name definition,
    set definition, and prize and set removal.

---
+++ src/main/java/uk/co/drnaylor/moneysigns/commands/CommandSignPrizes.java
@@ -0,0 +1,37 @@
+package uk.co.drnaylor.moneysigns.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import uk.co.drnaylor.moneysigns.MoneySigns;
+import uk.co.drnaylor.moneysigns.Util;
+import org.bukkit.ChatColor;
+
+/**
+ * Handles the /sp or /signprizes command.
+ *
+ * Syntax:
+ * /sp define <id> <timeout>
+ * /sp modify <id> <timeout>
+ * /sp <info|remove> <id>
+ * /sp defineset <id> <set info...|inventory>
+ * /sp modifyset <id> <set info...|inventory>
+ * /sp definesetmoney <id> <money>
+ * /sp <setinfo|setremove> <id> <set>
+ */
+public class CommandSignPrizes implements CommandExecutor {
+	
+	// Handles /sp or /signprizes.
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		
+		
+		
+		
+		
+		return false;
+	}
+	
+	
+	
+}
