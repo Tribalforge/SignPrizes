3f416ee32fe44f395cba2a173c8df4cee5a86805
Eevee Modevet <Eevables@hotmail.com>
2013-04-19 09:56:36 -0500

    Create CommandSPInfo.java for /spinfo.
    This command will be able to deal with an ID and a set name.
    It will simply return information about the specified ID and/or set, given the user has permission to see it!

---
+++ src/main/java/uk/co/drnaylor/moneysigns/commands/CommandSPInfo.java
@@ -0,0 +1,30 @@
+package uk.co.drnaylor.moneysigns.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import uk.co.drnaylor.moneysigns.MoneySigns;
+import uk.co.drnaylor.moneysigns.Util;
+import org.bukkit.ChatColor;
+
+/**
+ * Handles the /spinfo command.
+ *
+ * Syntax:
+ * /spinfo <id> [set]
+ * 
+ * Aliases: /signprizesinfo
+ * 
+ * Permission: signprizes.command.info.id
+ * Permission: signprizes.command.info.set
+ */
+public class CommandSPInfo implements CommandExecutor {
+  
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		
+		
+		return false;
+	}
+	
+}
