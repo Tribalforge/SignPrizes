1ad191a71396bcfad5c517524de3cd6173c474eb
Eevee Modevet <Eevables@hotmail.com>
2013-04-19 09:43:49 -0500

    Create CommandSPModify.java for /spmodify.
    This command will allow users to modify an existing prize ID.

---
+++ src/main/java/uk/co/drnaylor/moneysigns/commands/CommandSPModify.java
@@ -0,0 +1,29 @@
+package uk.co.drnaylor.moneysigns.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import uk.co.drnaylor.moneysigns.MoneySigns;
+import uk.co.drnaylor.moneysigns.Util;
+import org.bukkit.ChatColor;
+
+/**
+ * Handles the /spmodify command.
+ *
+ * Syntax:
+ * /spmodify <id> <new timeout>
+ * 
+ * Aliases: /signprizesmodify, /spmod, /signprizesmod
+ * 
+ * Permission: signprizes.id.modify
+ */
+public class CommandSPModify implements CommandExecutor {
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		
+		
+		return false;
+	}
+	
+}
