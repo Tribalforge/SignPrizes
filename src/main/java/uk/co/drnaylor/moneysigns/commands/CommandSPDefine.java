3d1482b56b29660400af37e2222bc024d8589952
Eevee Modevet <Eevables@hotmail.com>
2013-04-19 09:41:30 -0500

    Create CommandSPDefine.java for /spdefine.
    This command will allow users to define new prize identifiers.

---
+++ src/main/java/uk/co/drnaylor/moneysigns/commands/CommandSPDefine.java
@@ -0,0 +1,29 @@
+package uk.co.drnaylor.moneysigns.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import uk.co.drnaylor.moneysigns.MoneySigns;
+import uk.co.drnaylor.moneysigns.Util;
+import org.bukkit.ChatColor;
+
+/**
+ * Handles the /spdefine command.
+ *
+ * Syntax:
+ * /sp define <id> <timeout>
+ * 
+ * Aliases: /signprizesdefine, /spdef, /signprizesdef
+ * 
+ * Permission: signprizes.id.create
+ */
+public class CommandSPDefine implements CommandExecutor {
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		
+		
+		return false;
+	}
+	
+}
