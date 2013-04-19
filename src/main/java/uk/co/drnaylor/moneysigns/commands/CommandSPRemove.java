98aad14320e66d4b52aa5de84413bba83bfb7762
Eevee Modevet <Eevables@hotmail.com>
2013-04-19 10:13:15 -0500

    Create CommandSPRemove.java for /spremove.
    This command will remove either a specified set from a prize ID, or the entire prize ID itself.

---
+++ src/main/java/uk/co/drnaylor/moneysigns/commands/CommandSPRemove.java
@@ -0,0 +1,31 @@
+package uk.co.drnaylor.moneysigns.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import uk.co.drnaylor.moneysigns.MoneySigns;
+import uk.co.drnaylor.moneysigns.Util;
+import org.bukkit.ChatColor;
+
+/**
+ * Handles the /spremove command.
+ *
+ * Syntax:
+ * /spremove <id> [set]
+ * 
+ * Aliases: /signprizesremove, /sprem, /signprizesrem
+ * 
+ * Permissions:
+ * signprizes.commands.spremove.id
+ * signprizes.commands.spremove.set
+ */
+public class CommandSPRemove implements CommandExecutor {
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		
+		
+		return false;
+	}
+	
+}
